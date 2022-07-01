5d91f4fefd2c3094ee9c0235d070c2435051c425
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-01 12:44:38 -0400

    initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿string myName = "Nancy";
+int myAge = 40;
+
+
+Console.WriteLine($"My name is {myName} and I am {myAge} years old");
+
+char dollarSign = '$';
+
+Console.WriteLine($"Monetary values start with {dollarSign}.");
+
+bool hasBrownEyes = true;
+Console.WriteLine($"Has Brown Eyes: {hasBrownEyes}");
+
+double footSize = 7.5;
+
+Console.WriteLine($"My show size is {footSize}.");
+
+decimal piValue = 3.14m;
+
+Console.WriteLine($"The value of Pi is {piValue}."); 
+
